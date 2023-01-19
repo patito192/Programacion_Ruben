7c3fd7b006f8765c3e63a4ec113a22a9783cb143
pwnGIGO <51888730+pwnGIGO@users.noreply.github.com>
2023-01-19 16:41:29 -0600

    Revisión Practica 1 Fundamentos.

--- 1. Fundamentos de programación/Prácticas/Práctica 1, Primeros pasos/prac1/Hola en C/hola.c
+++ 1. Fundamentos de programación/Prácticas/Práctica 1, Primeros pasos/prac1/Hola en C/hola.c
@@ -1,8 +1,19 @@
+/*
+	Poner información del programador y poner que hace este programa
+	También escribir cómo se realiza la compilación y ejecución
+	----------------------------------------------------------------
+	Put the information of the programmer and put what do this program.
+	Futher, type how to compile and run the program
+*/
+
 #include <stdio.h>
 
-
+/*
+	La intentación esta muy bien!
+	-----------------------------
+	The indentation is nice!
+*/
 int main(){
 	printf("Hi :)\n");
-
 	return 0;
 }
--- 1. Fundamentos de programación/Prácticas/Práctica 1, Primeros pasos/prac1/Hola en java/hola.java
+++ 1. Fundamentos de programación/Prácticas/Práctica 1, Primeros pasos/prac1/Hola en java/hola.java
@@ -1,3 +1,16 @@
+/*
+	Poner información del programador y poner que hace este programa
+	También escribir cómo se realiza la compilación y ejecución
+	----------------------------------------------------------------
+	Put the information of the programmer and put what do this program.
+	Futher, type how to compile and run the program
+*/
+
+/*
+	Aquí la indentación se respeta muy bien
+	------------------------------------------------------
+	In here the indentation is respected very well in here
+*/
 public class hola{
 
 	public static void main(String[] args) {
