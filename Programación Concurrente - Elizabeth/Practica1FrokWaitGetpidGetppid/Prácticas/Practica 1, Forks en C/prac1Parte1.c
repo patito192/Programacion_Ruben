bec8b0c404d80aba60cb0379e54d99dc0da16332
patito192 <bicinrubenbr@gmail.com>
2023-02-20 16:47:44 -0600

    Update prac1Parte1.c

--- Programación Concurrente - Elizabeth/Practica1FrokWaitGetpidGetppid/Prácticas/Practica 1, Forks en C/prac1Parte1.c
+++ Programación Concurrente - Elizabeth/Practica1FrokWaitGetpidGetppid/Prácticas/Practica 1, Forks en C/prac1Parte1.c
@@ -4,22 +4,47 @@ Rubén Ramírez Cervantes
 bicinrubenbr@gmail.com
 
 Algoritmo:
-1-El programa muestra la fórmula cx^n
+1-El programa muestra la fórmula cx^n !
 2-el programa solicita el dato C al usuario
 3-El usuario ingresa el dato C
 4-el programa solicita el dato x al usuario
 5-El usuario ingresa el dato x
 6-el programa solicita el dato n al usuario
 7-El usuario ingresa el dato n
-8- el programa crea un proceso
-9-El proceso padre se queda esperando
-10-El proceso hijo calcula x^2
-11-El segundo proceso crea el tercer proceso
-12-EL proceso x calcula x^3
+8-Inicializar el resultado con X !
+9-Crear un contador !
+10-El programa crea un proceso !
+11-El proceso padre se queda esperando !
+12-El proceso hijo calcula x^2 y lo guarda en Resultado !
+13-Se repite el paso 8 hasta que se cumpla el valor de n !
+14-El programa multiplica el resultado por c
+
 */
 #include <stdio.h>
-int int main(int argc, char const *argv[])
-{
-	/* code */
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+//#include <sys/types.h>
+//#include <math.h>
+int main(int argc, char const *argv[]){
+	//Declaraciones
+	int coeficienteC=2, baseX=4, exponenteN=2, contador=0, resultado; //R 32
+	pid_t pidHijo;
+
+	resultado = baseX;
+
+	pidHijo = fork();
+
+	if (pidHijo != 0){
+		wait();
+	}
+	else{
+		resultado = pow(baseX, exponenteN);
+		printf("tu resultado es: %d", resultado);
+	}
+
+	printf("Tu fórmula es: cx^n\n");
+
+	printf("coeficiente: %d, base: %d, exponente: %d, contador: %d, resultado: %d", coeficienteC , baseX, exponenteN, contador, resultado);
 	return 0;
 }
