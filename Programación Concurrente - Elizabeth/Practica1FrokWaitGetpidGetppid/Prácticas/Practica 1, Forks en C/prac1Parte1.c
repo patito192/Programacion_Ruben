88e215e57eee1b01598ce7f4bd2a1f134366c80d
patito192 <bicinrubenbr@gmail.com>
2023-02-20 15:13:33 -0600

    Create prac1Parte1.c

---
+++ Programación Concurrente - Elizabeth/Practica1FrokWaitGetpidGetppid/Prácticas/Practica 1, Forks en C/prac1Parte1.c
@@ -0,0 +1,25 @@
+/*
+2203007463
+Rubén Ramírez Cervantes
+bicinrubenbr@gmail.com
+
+Algoritmo:
+1-El programa muestra la fórmula cx^n
+2-el programa solicita el dato C al usuario
+3-El usuario ingresa el dato C
+4-el programa solicita el dato x al usuario
+5-El usuario ingresa el dato x
+6-el programa solicita el dato n al usuario
+7-El usuario ingresa el dato n
+8- el programa crea un proceso
+9-El proceso padre se queda esperando
+10-El proceso hijo calcula x^2
+11-El segundo proceso crea el tercer proceso
+12-EL proceso x calcula x^3
+*/
+#include <stdio.h>
+int int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
